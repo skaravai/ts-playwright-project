b4d81383370485890c50dc7580889db686484233
skaravai <s.karavai@superdao.co>
2024-01-20 19:51:53 +0300

    refactoring locators

--- page-objects/projectPage.ts
+++ page-objects/projectPage.ts
@@ -1,16 +1,17 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class ProjectPage {
 
     readonly page: Page
+    readonly newCaseButton: Locator
 
     constructor(page: Page) {
         this.page = page
+        this.newCaseButton = page.locator('#create-case-button')
     }
 
     async clickCreateNewCaseButton(){
-        const newCaseButton = this.page.locator('#create-case-button')
         await this.page.waitForSelector('#create-case-button')
-        await newCaseButton.click()
+        await this.newCaseButton.click()
     }
 }
--- page-objects/projectsListPage.ts
+++ page-objects/projectsListPage.ts
@@ -1,25 +1,27 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 
 export class ProjectsListPage{
 
     readonly page: Page
+    readonly createNewProjectButton: Locator
+    readonly projectNameInput: Locator
+    readonly projectCodeInput: Locator
+    readonly createProjectButton: Locator
 
     constructor(page: Page){
         this.page = page
+        this.createNewProjectButton = page.locator('#createButton')
+        this.projectNameInput = page.locator('#project-name')
+        this.projectCodeInput = page.locator('#project-code')
+        this.createProjectButton = page.locator('span', { hasText: 'Create project' })
     }
 
     // This method creates a new project
-    
     async createNewProject() {
-        const createNewProjectButton = this.page.locator('#createButton')
-        const projectNameInput = this.page.locator('#project-name')
-        const projectCodeInput = this.page.locator('#project-code')
-        const createProjectButton = this.page.locator('span', { hasText: 'Create project' })
-        await createNewProjectButton.click()
-        await projectNameInput.fill(Math.random().toString(36).substring(2,7))
-        await projectCodeInput.fill(Math.random().toString(36).substring(3,6))
-        await createProjectButton.click()
-
+        await this.createNewProjectButton.click()
+        await this.projectNameInput.fill(Math.random().toString(36).substring(2,7))
+        await this.projectCodeInput.fill(Math.random().toString(36).substring(3,6))
+        await this.createProjectButton.click()
         await this.page.waitForSelector('h3:has-text("Create new project")')
     }
 }
